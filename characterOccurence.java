c0374d08dbed970ea8e8ea0cadfdab952f515fae
Balajisridhar3108 <92989836+Balajisridhar3108@users.noreply.github.com>
2022-02-15 19:46:22 +0530

    Add files via upload

---
+++ characterOccurence.java
@@ -0,0 +1,15 @@
+package week2.day2;
+
+public class characterOccurence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="welcome";
+	
+		String newstring=s.replace("e", "");
+		
+		System.out.println(s.length()-newstring.length());
+
+	}
+
+}
